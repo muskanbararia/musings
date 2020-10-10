9671f7151ed115faab737e49ac37f0d66d8f4c8f
Muskan Bararia <muskan.bararia@juspay.in>
2020-10-10 23:05:51 +0530

    Link worked

--- Markdowns/main.md
+++ Markdowns/main.md
@@ -1,3 +1,4 @@
 # This is a Minimalist Old School Blog
 
 ## Because deep down I am 60.
+### Click [here](main2.html) to check links
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var marked = require('marked');
 var fs = require('fs');
 
+
+// Read main 
 var readMe = fs.readFileSync('./Markdowns/main.md', 'utf-8');
 var markdownReadMe = marked(readMe);
 fs.mkdir('js', function(){
 fs.writeFileSync('./js/main.html', markdownReadMe);
 }
 )
+
+// main2
+var readMe2 = fs.readFileSync('./Markdowns/main2.md', 'utf-8');
+var markdownReadMe2 = marked(readMe2);
+fs.writeFileSync('./js/main2.html', markdownReadMe2);
